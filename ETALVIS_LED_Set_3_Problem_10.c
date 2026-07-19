3560ce891da497639d57d15b9ac9641a1df83853
Sivanesan S <sivanesans.23ece@gmail.com>
2026-07-20 05:23:58 +0530

    Add files via upload

---
+++ ETALVIS_LED_Set_3_Problem_10.c
@@ -0,0 +1,34 @@
+void make_delay(volatile long);
+
+void setup() {
+  volatile char *dir = (volatile char *)0x21;
+  volatile char *out = (volatile char *)0x22;
+
+  *dir = 0xFF;
+
+  int limit = 0;
+  
+  while (1)
+  {
+    *out = 0x00;
+    make_delay(100000);
+    limit = 7;
+
+    for (volatile int i = 0; i < 8; i++)
+    {
+      for (volatile int j = 7; j >= limit; j--)
+      {
+        *out |= (1 << j);
+         make_delay(80000);
+      }
+      *out = 0x00;
+      make_delay(100000);
+      limit--;
+    }
+  }
+}
+
+void make_delay(volatile long n)
+{
+  for (volatile long i = 0; i < n; i++);
+}
